3c49b2458f9c4555d670f58fc68918c3705fa417
Janio Isacura <janioisacura@gmail.com>
2021-11-30 15:13:22 -0300

    Styles and content update

--- 03-vueExercise/main.js
+++ 03-vueExercise/main.js
@@ -6,14 +6,14 @@ Vue.component("app-hobbies", {
     };
   },
   template: `
-    <ul>
-      <li class="u-li" v-for="(hobbie, index) in hobbies" v-on:click="removehobbie(hobbie, index)">{{ hobbie }}</li>
+    <ul class="unstyled-list">
+      <li class="pillow" v-for="(hobbie, index) in hobbies" v-on:click="removehobbie(hobbie, index)">{{ hobbie }}</li>
     </ul>
   `,
   methods: {
     removehobbie: function (hobbie, index) {
       //takes 2 arguments, the first is the custom name but in lowercase event, the second is the data you want to pass
-      this.$emit("removehobbie", {hobbie, index});
+      this.$emit("removehobbie", { hobbie, index });
       //console.log(hobbie, index)
     },
   },
@@ -24,12 +24,12 @@ new Vue({
   data: {
     hobbie: "",
     hobbies: [
-      "Smell my fingers after scratching my nuts",
-      "To fart in crowded and close places",
-      "Say awkward things in funerals",
-      "Cracking my fingers or finger my crack",
-      "Make weird noises at the gym",
-      "Pretend that I know what I'm talking about in work meetings",
+      "👃 Smell my fingers after scratching my nuts",
+      "🦨 To fart in crowded and close places",
+      "⚰️ Say awkward things in funerals",
+      "🖕 Cracking my fingers or finger my crack",
+      "😱 Make weird noises at the gym",
+      "🤥 Pretend that I know what I'm talking about in work meetings",
     ],
     removed: "",
     hide: true,
